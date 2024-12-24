dbc6625204d06c87f2e9894af95db322507c3952
Caio Carvalho <caio.carvalho@scheffer.agr.br>
2024-12-24 13:03:33 -0400

    refactor: move Pizza struct to models package and update handler function

--- main.go
+++ main.go
@@ -3,17 +3,12 @@ package main
 import (
 	"net/http"
 
+	"github.com/carvalhocaio/go-api-rest/models"
 	"github.com/gin-gonic/gin"
 )
 
-type Pizza struct {
-	ID    int     `json:"id"`
-	Nome  string  `json:"nome"`
-	Preco float64 `json:"preco"`
-}
-
-func pizzas(c *gin.Context) {
-	var pizzas = []Pizza{
+func getPizzas(c *gin.Context) {
+	var pizzas = []models.Pizza{
 		{ID: 1, Nome: "toscana", Preco: 23.5},
 		{ID: 2, Nome: "marguerita", Preco: 27.3},
 	}
@@ -23,7 +18,7 @@ func pizzas(c *gin.Context) {
 
 func main() {
 	r := gin.Default()
-	r.GET("/pizzas", pizzas)
+	r.GET("/pizzas", getPizzas)
 
 	r.Run()
 }
