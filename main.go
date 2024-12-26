2af97f994d89df653553388db15c03e764d4215f
Caio Carvalho <caio.carvalho@scheffer.agr.br>
2024-12-25 23:02:55 -0400

    feat: implement POST and GET by ID endpoints for pizzas

--- main.go
+++ main.go
@@ -2,23 +2,53 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/carvalhocaio/go-api-rest/models"
 	"github.com/gin-gonic/gin"
 )
 
+var pizzas = []models.Pizza{
+	{ID: 1, Nome: "toscana", Preco: 23.5},
+	{ID: 2, Nome: "marguerita", Preco: 27.3},
+}
+
 func getPizzas(c *gin.Context) {
-	var pizzas = []models.Pizza{
-		{ID: 1, Nome: "toscana", Preco: 23.5},
-		{ID: 2, Nome: "marguerita", Preco: 27.3},
+	c.JSON(http.StatusOK, gin.H{"pizzas": pizzas})
+}
+
+func postPizzas(c *gin.Context) {
+	var newPizza models.Pizza
+	if err := c.ShouldBindJSON(&newPizza); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro": err.Error()})
+		return
+	}
+	pizzas = append(pizzas, newPizza)
+}
+
+func getPizzasByID(c *gin.Context) {
+	idParam := c.Param("id")
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"erro": err.Error()})
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{"pizzas": pizzas})
+	for _, p := range pizzas {
+		if p.ID == id {
+			c.JSON(http.StatusOK, p)
+			return
+		}
+	}
+
+	c.JSON(http.StatusNotFound, gin.H{"message": "Pizza not found"})
 }
 
 func main() {
 	r := gin.Default()
 	r.GET("/pizzas", getPizzas)
+	r.POST("/pizzas", postPizzas)
+	r.GET("/pizzas/:id", getPizzasByID)
 
 	r.Run()
 }
